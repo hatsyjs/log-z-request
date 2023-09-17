1c859bb9144aa87f351df0d26321451c6968a352
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-09-17 15:13:13 +0700

    Updates

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKSi0sTS0uifLJT3fOz0vLTFfSUSpILMkASmXmlaQWpSUmpxbrY6jSyyjJzQEqzc7MS1GyMjI1q9WBGwlSlZ6Zh90kmCRpBpQlFmUmJuUg6dc1RDXB2Kg2FgB5D5wz1gAAAA=="
